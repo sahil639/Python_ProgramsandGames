7ef6d41bc6dbf6a9917b384c006f1ab9b4741808
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-08-26 00:17:24 +0530

    update

--- Python_ScreenPet/New folder/Python_screenpet.py
+++ Python_ScreenPet/New folder/Python_screenpet.py
@@ -11,10 +11,24 @@ body = c.create_oval(35,20,365,350,outline=c.body_color, fill=c.body_color)
 foot_left = c.create_oval(65 ,320,145,360, outline=c.body_color, fill=c.body_color)
 foot_right = c.create_oval(250 ,320,330,360, outline=c.body_color, fill=c.body_color)
 
-ear-left = c.create_polygon(75,80,75,10,165,70,outline=c.body_color, fill=c.body_color)
-ear-right = c.create_polygon(255,45,325,10,320,70,outline=c.body_color, fill=c.body_color)
+ear_left = c.create_polygon(75,80,75,10,165,70,outline=c.body_color, fill=c.body_color)
+ear_right = c.create_polygon(255,45,325,10,320,70,outline=c.body_color, fill=c.body_color)
 
-eye_left = c.create_oval(130,110,160,170, outline=c.white, fill=c.white)
+eye_left = c.create_oval(130,110,160,170, outline='black', fill='white')
+tupil_let = c.create_oval(140,145,150,155,outline='black', fill='black')
+eye_rigth = c.create_oval(230,110,260,170, outline='black', fill='white')
+tupil_right = c.create_oval(240,145,250,155,outline='black', fill='black')
+
+mouth_normal = c.create_line(170,250,200,272,230,250,smooth=1, width=2, state=NORMAL)
+mouth_happy = c.create_line(170,250,200,282,230,250,smooth=1, width=2, state=NORMAL)
+mouth_sad = c.create_line(170,250,200,232,230,250,smooth=1, width=2, state=NORMAL)
+
+tongue_mains = c.create_rectangle(170,250,230,290,outline='red', fill='red')
+tongue_tip = c.create_oval(170,285,230,300,outline='red', fill='red')
+
+cheek_left = c.create_oval(70,180,120,230,outline='pink', fill='pink')
+cheek_right = c.create_oval(280,180,330,230,outline='pink', fill='pink')
+
 c.pack()
 
 win.mainloop()
