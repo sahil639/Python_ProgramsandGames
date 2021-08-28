9dbfbb40993a3a64a64c4e0fd459ad5fb3aad345
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-08-28 13:00:47 +0530

    Update Python_SmartCalculator.py

--- Python_SmartCalculator/Python_SmartCalculator.py
+++ Python_SmartCalculator/Python_SmartCalculator.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 
+
 def add(a,b):
     return a + b
 
@@ -17,20 +18,51 @@ def mod(a,b):
     return a % b
 
 def lcm(a,b):
-    L = a if a > b else b
+    L = a if a>b else b
     while L <= a*b:
         if L%a == 0 and L%b == 0:
             return L
-
-        L +=1
+        L+=1
 
 def hcf(a,b):
-    H = a if a < b else b
+    H = a if a<b else b
     while H >= 1:
         if a%H == 0 and b%H == 0:
             return H
-        H -= 1
+        H-=1
 
+def extraxt_from_text(text):
+    l = []
+    for t in text.split(' '):
+        try:
+            l.append(float(t))
+        except ValueError:
+            pass
+    return l
+
+def calculate():
+    text = textin.get()
+    for word in text.split(' '):
+        if word.upper() in operations.keys():
+            try:
+                l = extraxt_from_text(text)
+                r = operations[word.upper()](l[0] , l[1])
+                list.delete(0,END)
+                list.insert(END,r)
+            except:
+                list.delete(0,END)
+                list.insert(END,'something went wrong please enter again')
+            finally:
+                break
+        elif word.upper() not in operations.keys():
+            list.delete(0,END)
+            list.insert(END,'something went wrong please enter again')
+
+operations = {'ADD':add, 'ADDITION':add , 'SUM':add , 'PLUS': add,
+                'SUB':sub , 'DIFFERENCE':sub , 'MINUS':sub , 'SUBTRACT':sub ,
+                'LCM':lcm, 'HCF':hcf, 'PRODUCT':mul, 'MULTIPLICATION':mul,
+                'MULTIPLY':mul, 'DIVISION':div, 'DIV':div , 'DIVIDE':div, 'MOD':mod, 
+                'REMAINDER':mod, 'MODULUS':mod }
 
 win = Tk()
 win.geometry('500x300')
@@ -49,8 +81,12 @@ textin = StringVar()
 e1 = Entry(win, width=30, textvariable=textin)
 e1.place(x=100,y=160)
 
-b1 =Button(win, text = 'Just this')
+b1 =Button(win, text = 'Just this' , command=calculate)
 b1.place(x=210, y=200)
 
 list = Listbox(win, width=20, height=3)
 list.place(x=150, y= 230)
+
+win.mainloop()
+
+
