b223565b8879656c4c72d1659cef708c9b75a2e6
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-09-08 22:38:13 +0530

    Update Python_PersonalDB.py

--- Python_PersonalDatabase/Python_PersonalDB.py
+++ Python_PersonalDatabase/Python_PersonalDB.py
@@ -1,8 +1,24 @@
 from tkinter import *
 import Python_backend
 
-def get_selected_row():
-    pass
+def get_selected_row(event):
+    global selected_row
+    index = list.curselection()[0]
+    selected_row = list.get(index)
+    e1.delete(0,END)
+    e1.insert(END, selected_row[1])
+    e2.delete(0,END)
+    e2.insert(END, selected_row[2])
+    e3.delete(0,END)
+    e3.insert(END, selected_row[3])
+    e4.delete(0,END)
+    e4.insert(END, selected_row[4])
+    e5.delete(0,END)
+    e5.insert(END, selected_row[5])
+    e6.delete(0,END)
+    e6.insert(END, selected_row[6])
+
+    
 
 
 def view_command():
@@ -14,6 +30,17 @@ def search_command():
     list.delete(0, END)
     for row in Python_backend.search(date_text.get(),earnings_text.get(),excercise_text.get(),study_text.get(),diet_text.get(),python_text.get()):
         list.insert(END, row)
+
+def add_command():
+    Python_backend.insert(date_text.get(),earnings_text.get(),excercise_text.get(),study_text.get(),diet_text.get(),python_text.get())
+    list.delete(0, END)
+    list.insert(END, (date_text.get(),earnings_text.get(),excercise_text.get(),study_text.get(),diet_text.get(),python_text.get()))
+
+def delete_command():
+    Python_backend.delete(selected_row[0])
+
+
+
 
 win = Tk()
 win.wm_title("My Routine Database")
@@ -61,21 +88,21 @@ list.grid(row=3, column=0, rowspan=9, columnspan=2)
 sb = Scrollbar(win)
 sb.grid(row=3,column=2,rowspan=9)
 
-list.bind('<ListBoxSelection>',get_selected_row)
+list.bind('<<ListBoxSelection>>',get_selected_row)
 
-b1 = Button(win, text= 'ADD', widh=12,pady=5)
+b1 = Button(win, text= 'ADD', width=12,pady=5, command=add_command)
 b1.grid(row=3, column=3)
 
-b2 = Button(win, text= 'Search', widh=12,pady=5, command=search_command)
+b2 = Button(win, text= 'Search', width=12,pady=5, command=search_command)
 b2.grid(row=4, column=3)
 
-b3 = Button(win, text= 'Delete Date', widh=12,pady=5)
+b3 = Button(win, text= 'Delete Date', width=12,pady=5, command=delete_command)
 b3.grid(row=5, column=3)
 
-b4 = Button(win, text= 'View all', widh=12,pady=5, command=view_command)
+b4 = Button(win, text= 'View all', width=12,pady=5, command=view_command)
 b4.grid(row=6, column=3)
 
-b5 = Button(win, text= 'Close', widh=12,pady=5,command= win.destroy)
+b5 = Button(win, text= 'Close', width=12,pady=5,command= win.destroy)
 b5.grid(row=7, column=3)
 
 
