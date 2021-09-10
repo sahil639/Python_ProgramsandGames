20531916084296a4af50046067e1c005f6807844
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-09-10 21:58:30 +0530

    Update Python_dictionary.py

--- Python_Dictionary/Python_dictionary.py
+++ Python_Dictionary/Python_dictionary.py
@@ -1,4 +1,5 @@
 import json 
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -10,10 +11,24 @@ def translate(word):
         return data(word.title())
     elif word.upper() in data:
         return data(word.upper())
-         
+    elif len(get_close_matches(word, data.keys())) > 0:
+        print("did you mean %s instead" %get_close_matches(word, data.keys()) [0])
+        decide = input("press y for y or n for no")
+        if decide == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif decide == "n":
+            return("Invalid word")
+        else:
+            print("Invalid choice")
+
+                   
     else:
         print("Invalid word")
 
 word = input("Enter the word you want to search \n")
 output = translate(word)
-print(output)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
